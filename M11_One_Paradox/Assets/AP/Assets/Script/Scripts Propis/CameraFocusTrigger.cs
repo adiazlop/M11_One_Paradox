da273ba005bbe0cb03f1dc0c37e34b4980256ea7
javier-marti-IOC <javimartiruiz@gmail.com>
2024-05-16 09:36:25 +0200

    javi

--- M11_One_Paradox/Assets/AP/Assets/Script/Scripts Propis/CameraFocusTrigger.cs
+++ M11_One_Paradox/Assets/AP/Assets/Script/Scripts Propis/CameraFocusTrigger.cs
@@ -5,70 +5,56 @@ using UnityEngine.Video;
 
 public class CameraFocusTrigger : MonoBehaviour
 {
-    public Camera otherCamera; // Referencia a la cámara que quieres activar
-    public MonoBehaviour componentToActivate; // Componente de script que quieres activar
     public GameObject[] gameObjectsToActivate; // Array de GameObjects que quieres activar
-    public GameObject[] gameObjectsToDeactivate; // Array de GameObjects que quieres desactivar
-    public float cameraActiveDuration = 43f; // Duración en segundos para mantener activada la cámara
+    public float activationDuration = 43f; // Duración en segundos para mantener activados los GameObjects
+    public Animator animatorToActivate; // Animator que quieres activar después del tiempo especificado
 
-    private bool cameraActive = false;
+    private bool objectsActivated = false;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // Verifica si el objeto que entró en el collider es el jugador
         {
-            // Activa la otra cámara si está disponible
-            if (otherCamera != null && !cameraActive)
-            {
-                otherCamera.gameObject.SetActive(true);
-                cameraActive = true;
-                Invoke("DeactivateCamera", cameraActiveDuration); // Desactiva la cámara después de un tiempo
-            }
-
             // Activa los GameObjects si están disponibles
-            foreach (GameObject go in gameObjectsToActivate)
+            if (!objectsActivated)
             {
-                if (go != null)
-                    go.SetActive(true);
-            }
-
-            // Desactiva los GameObjects si están disponibles
-            foreach (GameObject go in gameObjectsToDeactivate)
-            {
-                if (go != null)
-                    go.SetActive(false);
-            }
-
-            // Desactiva el componente si está disponible
-            if (componentToActivate != null)
-            {
-                componentToActivate.enabled = false;
-                Invoke("ActivateComponent", cameraActiveDuration); // Activa el componente después de un tiempo
+                ActivateObjects();
+                Invoke("DeactivateObjects", activationDuration); // Desactiva los GameObjects después de un tiempo
+                Invoke("ActivateAnimator", activationDuration); // Activa el Animator después del tiempo
             }
         }
     }
 
-    void DeactivateCamera()
+    void ActivateObjects()
     {
-        if (otherCamera != null)
+        foreach (GameObject go in gameObjectsToActivate)
         {
-            otherCamera.gameObject.SetActive(false);
-            cameraActive = false;
-
-            // Desactiva los GameObjects si están disponibles
-            foreach (GameObject go in gameObjectsToActivate)
-            {
-                if (go != null)
-                    go.SetActive(false);
-            }
+            if (go != null)
+                go.SetActive(true);
         }
+
+        objectsActivated = true;
     }
 
-    void ActivateComponent()
+    void DeactivateObjects()
     {
-        if (componentToActivate != null)
+        // Desactiva los GameObjects si están disponibles
+        foreach (GameObject go in gameObjectsToActivate)
         {
-            componentToActivate.enabled = true;
+            if (go != null)
+                go.SetActive(false);
+        }
+
+        objectsActivated = false;
+    }
+
+    void ActivateAnimator()
+    {
+        if (animatorToActivate != null)
+        {
+            animatorToActivate.enabled = true;
+            animatorToActivate.Play("barrera"); // Reemplaza "YourAnimationName" por el nombre de tu animación
         }
     }
+
 }
